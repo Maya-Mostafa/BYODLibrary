90311dd0999efe9c028c32eb0612234e05ff8d8f
Mai Mostafa <mayamostafa@gmail.com>
2024-02-23 15:36:06 -0500

    main component features

--- src/webparts/byodLibrary/components/ByodLibrary.tsx
+++ src/webparts/byodLibrary/components/ByodLibrary.tsx
@@ -1,43 +1,161 @@
 import * as React from 'react';
 import styles from './ByodLibrary.module.scss';
+import './ByodLibrary.scss';
 import { IByodLibraryProps } from './IByodLibraryProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { getGraphMemberOf, isFromTargetAudience, groupBy, getListItemsGraph } from '../services/requests';
+// import { escape } from '@microsoft/sp-lodash-subset';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import LibraryItem from './LibraryItem/LibraryItem';
+import Search from './Search/Search';
 
-export default class ByodLibrary extends React.Component<IByodLibraryProps, {}> {
-  public render(): React.ReactElement<IByodLibraryProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
+export default function ByodLibrary(props: IByodLibraryProps) {
 
-    return (
-      <section className={`${styles.byodLibrary} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
+  const [showBasedOnTargetAudience, setShowBasedOnTargetAudience] = React.useState(false);
+  const [items, setItems] = React.useState([]);
+  const [filteredItems, setFilteredItems] = React.useState(items);
+  const [isExp, setIsExp] = React.useState(props.isExp && props.isCollapsible);
+  const [memberOfGroups, setMemberofGroups] = React.useState(null);
+  
+  const [categories, setCategories] = React.useState([]);
+  const [categItems, setCategItems] = React.useState(null);
+  const [filteredCategItems, setFilteredCategItems] = React.useState(categItems);
+
+  React.useEffect(()=>{
+    console.log("props.context", props.context);
+
+    getListItemsGraph(props.context).then(res => {
+      console.log("graph", res);
+      if (props.groupBy){
+        const groupedArr = groupBy(res, props.groupByField);
+        setCategItems(groupedArr);
+        setFilteredCategItems(groupedArr);
+        setCategories(Object.keys(groupedArr));
+      }
+      setItems(res);
+      setFilteredItems(res);
+    });
+    getGraphMemberOf(props.context).then((res: any) => {
+      console.log("grpahMemberOf", res);
+      setMemberofGroups(res);
+    });
+
+    /*getListItems(props.context, props.siteUrl, props.listName).then(res => {
+      
+      if (props.groupBy){
+        const groupedArr = groupBy(res, props.groupByField);
+        setCategItems(groupedArr);
+        setFilteredCategItems(groupedArr);
+        setCategories(Object.keys(groupedArr));
+        
+        console.log("groupBy", groupedArr);
+        console.log("Object.keys(res)", Object.keys(groupedArr))
+      }
+
+      setItems(res);
+      setFilteredItems(res);
+    });*/
+
+    if (props.targetAudience && props.targetAudience.length > 0){
+      setShowBasedOnTargetAudience(isFromTargetAudience(props.context, memberOfGroups, props.targetAudience, 'fullName'));
+    }else{
+      setShowBasedOnTargetAudience(true);
+    }
+  }, []);
+
+  const onSearchChanged = (_: any, text: string): void => {
+    if (props.groupBy) setFilteredCategItems(items.filter(item => item.Title.toLowerCase().indexOf(text.toLowerCase()) >= 0));
+    else setFilteredItems(items.filter(item => item.fields.Title.toLowerCase().indexOf(text.toLowerCase()) >= 0));
+  };
+  
+
+  return (
+    <>
+    {showBasedOnTargetAudience &&
+      <section className={`${styles.byodLibrary} ${props.hasTeamsContext ? styles.teams : ""}`}>
+        {/* <div>Web part property value:{" "} <strong>{escape(props.description)}</strong></div>
+        <img alt='' className={styles.welcomeImage}
+          src={
+            props.isDarkTheme
+              ? require("../assets/welcome-dark.png")
+              : require("../assets/welcome-light.png")
+          }
+        /> */}
+
+        <div className={styles.main}>
+          <div className={styles.librarySection} style={{borderBottom: props.showDivider ? '1px solid #ddd' : '1px solid #fff'}}>
+            <h6 className={styles.sectionHdr} style={{color: props.color, borderBottomColor: props.color}} onClick={()=> props.isCollapsible && setIsExp(prev=>!prev)}>
+              <span>{props.sectionTitle ? props.sectionTitle : props.listName}</span>
+              {props.isCollapsible &&
+                <span className={props.iconAlignment ? styles.colExpRight : styles.colExpLeft}>
+                  {isExp ?
+                    <FontAwesomeIcon icon={faChevronDown} />
+                    :
+                    <FontAwesomeIcon icon={faChevronUp} />
+                  }
+                </span>
+              }
+            </h6>
+            
+            <div style={{display: props.isCollapsible ? (isExp ? 'block' : 'none') : 'block'}}>
+              <div>{props.sectionDescription}</div>
+              {props.enableSearch &&
+                <Search 
+                  searchPlaceholder={props.searchPlaceholder}
+                  onSearchChanged={onSearchChanged}
+                />
+              }
+              {props.groupBy ?
+                categories.map((categ: any) => {
+                  return(
+                    <div key={categ}>
+                      <h5 className={styles.secCategory}>{categ}</h5>
+                      <ul className={styles.cards}>
+                        {filteredCategItems[categ].map((item: any) => {
+                          return(
+                            <LibraryItem 
+                              item={item.fields} 
+                              customImgPicker={props.customImgPicker} 
+                              iconPicker={props.iconPicker} 
+                              thumbnail={props.thumbnail} 
+                              key={item.fields.id} 
+                          />
+                          )
+                        })}
+                      </ul>
+                    </div>
+                  );
+                })
+                :
+                <ul className={styles.cards}>
+                  {filteredItems.map((item: any)=>{
+                    if (!props.enableTargetAudience || 
+                        !item.fields._ModernAudienceTargetUserField || 
+                        props.enableTargetAudience && 
+                        memberOfGroups && 
+                        item.fields._ModernAudienceTargetUserField && 
+                        isFromTargetAudience(props.context, memberOfGroups, item.fields._ModernAudienceTargetUserField, 'LookupValue')){
+                      return(                                            
+                        <LibraryItem 
+                          item={item.fields} 
+                          customImgPicker={props.customImgPicker} 
+                          iconPicker={props.iconPicker} 
+                          thumbnail={props.thumbnail} 
+                          key={item.fields.id} 
+                        />
+                      )
+                    }
+                  })}
+                </ul>
+              }
+
+            </div>
+          </div>
         </div>
       </section>
-    );
-  }
+    }
+    </>
+  );
+
 }
+
--- src/webparts/byodLibrary/components/IByodLibraryProps.ts
+++ src/webparts/byodLibrary/components/IByodLibraryProps.ts
@@ -1,7 +1,31 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+
 export interface IByodLibraryProps {
   description: string;
   isDarkTheme: boolean;
   environmentMessage: string;
   hasTeamsContext: boolean;
   userDisplayName: string;
+
+  context: WebPartContext;
+  targetAudience: any;
+  siteUrl: string;
+  listName: string;
+  isExp: boolean;
+  color: string;
+  openInNewTab: boolean;
+  showDivider: boolean;
+  sectionTitle: string;
+  isCollapsible: boolean;
+  iconAlignment: string;
+  iconPicker: any;
+  thumbnail: any;
+  customImgPicker: any;
+
+  groupBy: boolean;
+  groupByField: string;
+  sectionDescription: string;
+  enableSearch: boolean;
+  searchPlaceholder: string;
+  enableTargetAudience: boolean;
 }
