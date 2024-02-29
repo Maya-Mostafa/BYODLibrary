f1a07651a1477ce5ccf4fce3584025216d4ddfa7
Mai Mostafa <mayamostafa@gmail.com>
2024-02-29 15:30:16 -0500

    target audience & delete fnc

--- src/webparts/byodLibrary/services/requests.ts
+++ src/webparts/byodLibrary/services/requests.ts
@@ -1,5 +1,6 @@
 import { WebPartContext } from "@microsoft/sp-webpart-base";
-import {SPHttpClient} from "@microsoft/sp-http";
+import {SPHttpClient, ISPHttpClientOptions} from "@microsoft/sp-http";
+import { SPPermission } from "@microsoft/sp-page-context";
 
 import { spfi, SPFx } from "@pnp/sp";
 import "@pnp/sp/webs";
@@ -27,13 +28,13 @@ export const getGraphMemberOf = async (context: WebPartContext) =>{
     return userGroups;
 };
 
-export const isFromTargetAudience = (context: WebPartContext, userGroups: any, targetAudience: any, targetAudienceKey: string) => {
+export const isFromTargetAudience = (userEmail: string, userGroups: any, targetAudience: any, targetAudienceKey: string) => {
     
     console.log("userGroups", userGroups);
     console.log("targetAudience", targetAudience);
 
     for (const audience of targetAudience){
-        if (context.pageContext.user.email === audience.email) return true;
+        if (userEmail === audience.email) return true;
         if (userGroups[audience[targetAudienceKey]]) return true;
     }
 
@@ -60,9 +61,25 @@ export const getListItemsCamlQuery = async (context: WebPartContext, siteUrl: st
 
 }   
 
-export const getListItemsGraph = async (context: WebPartContext) => {
+const getSiteId = async (context: WebPartContext, siteUrl: string) =>{
+    const responseUrl = `${siteUrl}/_api/site/id`;
+    const response = await context.spHttpClient.get(responseUrl, SPHttpClient.configurations.v1).then(r => r.json());
+    return response.value;
+};
+
+const getListGuid  = async (context: WebPartContext, siteUrl: string, listName: string) => {
+    const responseUrl = `${siteUrl}/_api/web/lists/getByTitle('${listName}')/Id`;
+    const response = await context.spHttpClient.get(responseUrl, SPHttpClient.configurations.v1).then(r => r.json());
+    return response.value;
+};
+
+export const getListItemsGraph = async (context: WebPartContext, siteUrl: string, listName: string) => {
+
+    const siteId = await getSiteId(context, siteUrl);
+    const listGuid = await getListGuid(context, siteUrl, listName);
+
     const graphClient = await context.msGraphClientFactory.getClient('3');
-    const items = await graphClient.api('sites/e9bb6985-442f-447d-9370-02bf2f404fb7/lists/524029f0-247c-446b-a811-b3f058dcec79/items?expand=fields(select=Title,link,Image,_ModernAudienceTargetUserField,Author,Id,login,pwd,LoginDisclaimer,NewTab,Category,ID,Created,Modified,Short_x0020_Description)').get();
+    const items = await graphClient.api(`sites/${siteId}/lists/${listGuid}/items?expand=fields(select=Title,link,Image,_ModernAudienceTargetUserField,Author,Id,login,pwd,LoginDisclaimer,NewTab,Category,ID,Created,Modified,Short_x0020_Description)`).get();
     return items.value;
 }
 
@@ -82,4 +99,30 @@ export const groupBy = (objArr: any, key: string) => {
         (rv[x[key]] = rv[x[key]] || []).push(x);
         return rv;
     }, {});
+};
+
+
+export const isUserManage = (context: WebPartContext) : boolean =>{
+    const userPermissions = context.pageContext.web.permissions,
+        permission = new SPPermission (userPermissions.value);
+    
+    return permission.hasPermission(SPPermission.manageWeb);
+};
+
+export const deleteItem = async (context: WebPartContext, siteUrl: string, listTitle: string, itemId: any) =>{
+    const restUrl = `${siteUrl}/_api/web/lists/getByTitle('${listTitle}')/items(${itemId})`;
+    const spOptions: ISPHttpClientOptions = {
+        headers:{
+            Accept: "application/json;odata=nometadata", 
+            "Content-Type": "application/json;odata=nometadata",
+            "odata-version": "",
+            "IF-MATCH": "*",
+            "X-HTTP-Method": "DELETE"         
+        },
+    };
+
+    const _data = await context.spHttpClient.post(restUrl, SPHttpClient.configurations.v1, spOptions);
+    if (_data.ok){
+        console.log('Tile is deleted!');
+    }
 };
